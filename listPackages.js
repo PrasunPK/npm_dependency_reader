f12f3d667f522479cde8e5b017a8027aae514de1
PrasunPK <prasun_1993@rediffmail.com>
2018-03-14 00:07:42 +0530

    Show all dependencies of a package

---
+++ listPackages.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const packageContent = fs.readFileSync('./package.json','UTF-8');
+const jsonData = JSON.parse(packageContent);
+
+const dependencies = jsonData.dependencies;
+
+//console version
+// ----------------------------------
+// | component name      | version  |
+// ----------------------------------
+// | comp 1              | 0.3.2    |
+// | comp 2              | 1.3.1    |
+// | comp 4              | 0.3.4    |
+// ----------------------------------
+
+const horizontalSeparator = "-";
+const verticalSeparator = "|";
+
+const dependencyNames = Object.keys(dependencies);
+const dependencyVersions = Object.values(dependencies);
+
+const findLongestLength = (collection) => {
+  var longest = collection.reduce(function (a, b) { return a.length > b.length ? a : b; });
+  return longest.length;
+};
+
+const addSpace = (length) => {
+  let spaces = '';
+  for (var i = 0; i < length; i++) {
+    spaces += ' ';
+  }
+  return spaces;
+}
+
+const getnameWithPadding = (name, collection, extra) => {
+  return name + addSpace(!extra ? (findLongestLength(collection) - name.length) : 2);
+};
+
+const calculateTotalLengthForHeaderBorder = () => {
+  return findLongestLength(dependencyNames) + findLongestLength(dependencyVersions) + 7;
+};
+
+const createTopBorder = () => {
+  let border = horizontalSeparator;
+  for (var i = 0; i < calculateTotalLengthForHeaderBorder(); i++) {
+    border += horizontalSeparator;
+  }
+  return border += horizontalSeparator;
+};
+
+const createColumnHeading = (name, extra) => {
+  return getnameWithPadding(name , dependencyNames, extra);
+}
+
+const createHeader = () => {
+  return createTopBorder() + '\n' + verticalSeparator +
+    addSpace(1) + createColumnHeading('Package Name') + addSpace(1) + verticalSeparator +
+    addSpace(1) + createColumnHeading('Version', true) + addSpace(0) + verticalSeparator + '\n' +
+    createTopBorder() + '\n' ;
+}
+
+let text = createHeader();
+for (var i = 0; i < dependencyNames.length; i++) {
+  text += verticalSeparator + addSpace(1) + getnameWithPadding(dependencyNames[i], dependencyNames) + addSpace(1) + verticalSeparator +
+                       addSpace(1) + getnameWithPadding(dependencyVersions[i], dependencyVersions, true) + addSpace(1) + verticalSeparator + '\n';
+}
+text+=createTopBorder();
+
+console.log(text);
