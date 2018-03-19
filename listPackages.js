7611a700be906b01ff46a1b8e4b33fcde3dc987f
PrasunPK <prasun_1993@rediffmail.com>
2018-03-19 22:24:06 +0530

    writing to a markdown

--- listPackages.js
+++ listPackages.js
@@ -12,51 +12,11 @@ const verticalSeparator = "|";
 const dependencyNames = Object.keys(dependencies).concat(Object.keys(peerDependencies));
 const dependencyVersions = Object.values(dependencies).concat(Object.values(peerDependencies));
 
-const findLongestLength = (collection) => {
-  var longest = collection.reduce(function (a, b) { return a.length > b.length ? a : b; });
-  return longest.length;
-};
-
-const addSpace = (length) => {
-  let spaces = '';
-  for (var i = 0; i < length; i++) {
-    spaces += ' ';
-  }
-  return spaces;
-}
-
-const getnameWithPadding = (name, collection, extra) => {
-  return name + addSpace(!extra ? (findLongestLength(collection) - name.length)+5 : 6);
-};
-
-const calculateTotalLengthForHeaderBorder = () => {
-  return findLongestLength(dependencyNames) + findLongestLength(dependencyVersions) + 7;
-};
-
-const createTopBorder = () => {
-  let border = horizontalSeparator;
-  for (var i = 0; i < calculateTotalLengthForHeaderBorder()+8; i++) {
-    border += horizontalSeparator;
-  }
-  return border += horizontalSeparator;
-};
-
-const createColumnHeading = (name, extra) => {
-  return getnameWithPadding(name , dependencyNames, extra);
-}
-
-const createHeader = () => {
-  return createTopBorder() + '\n' + verticalSeparator +
-    addSpace(1) + createColumnHeading('Package Name') + addSpace(1) + verticalSeparator +
-    addSpace(1) + createColumnHeading('Version', true) + addSpace(0) + verticalSeparator + '\n' +
-    createTopBorder() + '\n' ;
-}
-
-let text = createHeader();
+const header = '|Package Name|Version|\n'
+const horizontalBorder = '|:-|:-|';
+let text = header + horizontalBorder + '\n';
 for (var i = 0; i < dependencyNames.length; i++) {
-  text += verticalSeparator + addSpace(1) + getnameWithPadding(dependencyNames[i], dependencyNames) + addSpace(1) + verticalSeparator +
-                       addSpace(1) + getnameWithPadding(dependencyVersions[i], dependencyVersions, false) + addSpace(1) + verticalSeparator + '\n';
+  text += verticalSeparator + dependencyNames[i] + verticalSeparator + dependencyVersions[i] + verticalSeparator +'\n';
 }
-text+=createTopBorder();
 
 console.log(text);
